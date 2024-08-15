220fddcba87f4f121d276dc8122f80c6b9b35e34
rene valenz <renerviox@gmail.com>
2024-08-15 00:00:13 -0700

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import './App.css'
 import Card from './components/Card'
 
+
 function App() {
  
   return (
    <div className='App'>
     <h1>Carga de estudiantes</h1>
-    <form></form>
+    <form>
+    <div className="container">
+  <div className="left">
+    <div className="header">
+      <h2 className="animation a1">Bienvenid@ a clases!</h2>
+      <h4 className="animation a2">Inicia sesion con tu email y password</h4>
+    </div>
+    <div className="form">
+      <input type="email" className="form-field animation a3" placeholder="Email"/>
+      <input type="password" className="form-field animation a4" placeholder="Password"/>
+      <p className="animation a5"><a href="#">Forgot Password</a></p>
+      <button className="animation a6">Entrar</button>
+    </div>
+  </div>
+  <div className="right"></div>
+</div>
+    </form>
+    <br />
     <Card />
    </div>
   )
